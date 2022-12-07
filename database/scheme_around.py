41d04e6b0f92498eed7d8aac1c915aecb555c3ea
SeoSiun <siun0816@hanyang.ac.kr>
2022-12-07 21:18:22 +0900

    User, Evaluation Model 추가

--- database/scheme_around.py
+++ database/scheme_around.py
@@ -1,4 +1,3 @@
-
 from sqlmodel import Field, SQLModel
 
 
@@ -7,3 +6,19 @@ class Hero(SQLModel, table=True):
     name: str
     secret_name: str
     age: int | None = None
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    kakao_id: int = Field(unique=True)
+    nickname: str
+    image: str
+
+
+class Evaluation(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    evaluator: int = Field(foreign_key='user.id')
+    evaluated_user: int = Field(foreign_key='user.id')
+    promise_id: int = Field(foreign_key="promise.id")
+    content: str
+    star: int
