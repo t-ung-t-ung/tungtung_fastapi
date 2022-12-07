eeeac81d473286d5a8f8bc365b6de55277b65553
Rudolf0328 <ck07160@hanyang.ac.kr>
2022-12-07 21:15:05 +0900

    a

--- database/scheme_around.py
+++ database/scheme_around.py
@@ -1,9 +1,28 @@
+from datetime import datetime
 
 from sqlmodel import Field, SQLModel
-
 
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     secret_name: str
     age: int | None = None
+
+class Banner(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    image: str
+    link: str
+
+class Chat(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    type: int
+    time: datetime
+    content: str
+    user_id: int = Field(foreign_key='user.id')
+    promise_id: int = Field(foreign_key='promise.id')
+
+class UserPromise(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='user.id')
+    promise_id: int = Field(foreign_key='promise.id')
+    is_auth: bool
