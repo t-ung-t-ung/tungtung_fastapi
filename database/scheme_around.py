28c7f78d13d1537c4bf6ee2f0e3bec65e9e6e678
recsater <recsater@naver.com>
2022-12-07 21:14:46 +0900

    develop

--- database/scheme_around.py
+++ database/scheme_around.py
@@ -1,9 +1,18 @@
+from datetime import datetime
 
 from sqlmodel import Field, SQLModel
 
 
-class Hero(SQLModel, table=True):
+class Promise(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str
-    secret_name: str
-    age: int | None = None
+    owner: int = Field(foreign_key="user.id")
+    category_id: int = Field(foreign_key="category.id")
+    title: str
+    detail: str
+    latitude: float
+    longitude: float
+    promise_time: datetime
+    image: str
+    max_people: int
+    status: int
+
