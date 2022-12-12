bf0a72fb049955dd56b2705c0ad56d9393de3aaa
recsater <recsater@naver.com>
2022-12-12 17:08:09 +0900

    git hook

--- .githooks/_before_commit.py
+++ .githooks/_before_commit.py
@@ -4,9 +4,9 @@ with open("env.yaml", "a") as env:
     with open("temp.yaml") as file:
         k = False
         for line in file.readlines():
-            if line.find("- pip:") != -1:
-                k = True
             if k:
                 env.write(line)
+            if line.find("- pip:") != -1:
+                k = True
         os.remove("temp.yaml")
 
