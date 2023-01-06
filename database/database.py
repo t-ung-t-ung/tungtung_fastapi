753d1a7d318c9d59a45b41fbe5f814693cdd18a7
SeoSiun <siun0816@hanyang.ac.kr>
2023-01-06 14:08:02 +0900

    create_notification 함수를 구현했습니다.

--- database/database.py
+++ database/database.py
@@ -1,8 +1,6 @@
 from sqlmodel import create_engine, SQLModel, Session, select
-from database import scheme_around
-from database.scheme_around import User, Category, UserPromise
 
-from database.scheme_around import User, Category
+from database.scheme_around import User, UserPromise, Notification
 
 engine = create_engine(
     f'mysql+pymysql://siun:tldjsWkd!123@13.125.114.46:3306/around',
@@ -21,7 +19,13 @@ def get_user(user_id: str):
         user = session.exec(statement).one_or_none()
         return user
 
+
 def get_participants(promise_id: int, session: Session):
     statement = select(User).join(UserPromise).where(UserPromise.promise_id == promise_id)
     users = session.exec(statement).all()
     return users
+
+
+def create_notification(notification: Notification, session: Session):
+    session.add(notification)
+    session.commit()
