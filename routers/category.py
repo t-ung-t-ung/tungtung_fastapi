dd8e1fe5a42b1b2dcbf83d7eb12557ab859cd955
SeoSiun <siun0816@hanyang.ac.kr>
2023-01-06 15:47:10 +0900

    category patch api를 구현했습니다.

--- routers/category.py
+++ routers/category.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Body
+from fastapi import APIRouter, status, Body, HTTPException
 from sqlmodel import Session, select
 
 from database.scheme_around import Category
@@ -26,3 +26,19 @@ async def create_category(category: Category = Body(
         session.commit()
         session.refresh(category)
         return category
+
+
+@router.patch("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def create_category(category_id: int, category: Category = Body(
+    example=Category(name="운동").json(exclude_none=True)
+)):
+    with Session(engine) as session:
+        db_category = session.get(Category, category_id)
+        if not db_category:
+            raise HTTPException(status_code=404, detail="Category not found")
+        category_data = category.dict(exclude_unset=True)
+        for key, value in category_data.items():
+            setattr(db_category, key, value)
+        session.add(db_category)
+        session.commit()
+        session.refresh(db_category)
