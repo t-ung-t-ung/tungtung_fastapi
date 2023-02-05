03781eefba74b29d06d192966529735c4b0fae9d
Rudolf0328 <54528011+Rudolf0328@users.noreply.github.com>
2023-02-05 14:57:39 +0900

    Merge pull request #10
    * GET promise/의 response 값에 title, max_people, current_people을 추가하였습니다.
    
    * GET promise들의 owner의 정보를 다 주고 참여한 사람들의 정보를 다 주도록 수정했습니다.
    
    * GET promise 에서 현재 참가자 수를 찾는 방법을 바꿨습니다.
    
    * GET promise 에서 현재 참가자 수를 찾는 방법을 바꿨습니다.
    
    * correct 'get_promises()'
    
    * git hook
    
    * Merge remote-tracking branch 'origin/feature/1_api/promise' into feat…
    
    * get_promise(): sum이 아니라 count죠.
    
    * GET promise를 변경했습니다.
    
    * Merge remote-tracking branch 'origin/develop' into feature/1_api/promise
    
    * GET promise를 변경했습니다.
    
    * POST promise를 만들었습니다.
    
    * PUT promise를 만들었습니다.
    
    * DELETE promise/{promise_id}를 만들었습니다.
    
    * update_promise의 request와 parameter를 수정하였습니다.
    
    * put -> patch로 변경하였습니다.
    
    * Merge remote-tracking branch 'origin/develop' into feature/1_api/promise
    
    * promise api들을 변경했습니다.
    
    * apromise의 참가 신청, 수락, 거절과 관련된 api들을 만들었습니다.

--- routers/promise.py
+++ routers/promise.py
@@ -9,40 +9,26 @@ router = APIRouter(
     prefix="/promise"
 )
 
+
 class UserResponse(SQLModel):
     id: int
     nickname: str
     image: str
 
-class AllPromise(SQLModel):
-    detail: str
-    owner: int
-    category_id: int
-    longitude: float
-    image: str
-    status: int
-    title: str
-    id: int
-    latitude: float
-    promise_time: datetime
-    max_people: int
+
+class AllPromise(Promise, table=False):
     people: int
 
-class OnePromise(SQLModel):
-    detail: str
-    owner: UserResponse
-    category: Category
-    longitude: float
-    image: str
-    status: int
-    title: str
-    id: int
-    latitude: float
-    promise_time: datetime
-    max_people: int
+
+# class OnePromise(Promise, table=False):
+#     owner_info: UserResponse
+#     category_info: Category
+# people: list[UserResponse]
+
 
 class Result(SQLModel):
     result: int
+
 
 @router.get("/", response_model=list[AllPromise], status_code=status.HTTP_200_OK)
 async def get_promises():
@@ -62,28 +48,28 @@ async def get_promises():
         return output
 
 
-@router.get("/{promise_id}", response_model=OnePromise, status_code=status.HTTP_200_OK)
+@router.get("/{promise_id}", status_code=status.HTTP_200_OK)
 async def get_promise(promise_id: int):
     with Session(engine) as session:
-        print(type(session))
-        statement = select(Promise, User, Category).join(User, isouter=False).join(Category, isouter=True).where(Promise.id == promise_id)
+        statement = select(Promise, User, Category).join(User, isouter=False).join(Category, isouter=True).where(
+            Promise.id == promise_id)
         result = session.exec(statement)
         for promise, user, category in result:
             new_promise: dict = promise.dict()
-            del(new_promise['owner'])
-            del(new_promise['category_id'])
+            del (new_promise['owner'])
+            del (new_promise['category_id'])
             new_user: dict = user.dict()
-            del(new_user['kakao_id'])
+            del (new_user['kakao_id'])
             new_category: dict = category.dict()
-            print(new_category)
-            new_promise['owner'] = new_user
-            new_promise['category'] = new_category
+            new_promise['owner_info'] = new_user
+            new_promise['category_info'] = new_category
         participants = get_participants(new_promise['id'], session)
         new_promise['people'] = participants
         return new_promise
 
+
 @router.post("/", response_model=Promise, status_code=status.HTTP_201_CREATED)
-async def create_promise(newPromise: Promise = Body(
+async def create_promise(new_promise: Promise = Body(
     example=Promise(
         owner=1,
         category_id=1,
@@ -91,17 +77,37 @@ async def create_promise(newPromise: Promise = Body(
         detail="자세한 내용",
         latitude=10.0,
         longitude=10.0,
-        promise_time="2000-00-00T00:00:00",
+        address="상세주소",
+        start_time="2000-00-00T00:00:00",
+        end_time="2000-00-00T00:00:00",
         image="image.img",
         max_people=5,
         status=0
     ).json()
 )):
     with Session(engine) as session:
-        session.add(newPromise)
+        session.add(new_promise)
         session.commit()
-        session.refresh(newPromise)
-        return newPromise
+        session.refresh(new_promise)
+        return new_promise
+
+
+@router.post("/apply")
+async def apply_promise(user_promise: UserPromise = Body(
+    example=UserPromise(
+        user_id=1,
+        promise_id=1,
+        is_auth=0,
+        start_time='2000-00-00T00:00:00',
+        end_time='2000-00-00T00:00:00'
+    ).json()
+)):
+    user_promise.status = 2
+    with Session(engine) as session:
+        session.add(user_promise)
+        session.commit()
+        return {"result": 1}
+
 
 @router.patch("/", response_model=Promise, status_code=status.HTTP_200_OK)
 async def update_promise(promise: Promise):
@@ -119,20 +125,42 @@ async def update_promise(promise: Promise):
         session.refresh(current_promise)
         return current_promise
 
+
+@router.patch("/apply/{option}")
+async def apply_promise(option: int, user_promise: UserPromise = Body(
+    example=UserPromise(
+        user_id=1,
+        promise_id=1,
+        is_auth=0,
+    ).json()
+)):
+    with Session(engine) as session:
+        statement = select(UserPromise).where(UserPromise.promise_id == user_promise.promise_id,
+                                              UserPromise.user_id == user_promise.user_id)
+        current_user_promise = session.exec(statement).one()
+
+        print(current_user_promise)
+
+        if option == 1:
+            setattr(current_user_promise, "status", 1)
+        elif option == 0:
+            setattr(current_user_promise, "status", 0)
+
+        session.add(current_user_promise)
+        session.commit()
+
+        return {"result": 1}
+
+
 @router.delete("/{promise_id}", response_model=Result, status_code=status.HTTP_200_OK)
 async def delete_promise(promise_id: int):
     with Session(engine) as session:
         promise = session.get(Promise, promise_id)
-        user_promises = session.exec(select(UserPromise).where(UserPromise.promise_id == promise_id)).all()
 
         if not promise:
             raise HTTPException(status_code=404, detail="Promise not found")
 
-        for user_promise in user_promises:
-            session.delete(user_promise)
-            session.commit()
-
-        session.delete(promise)
+        setattr(promise, "status", -1)
         session.commit()
 
         return {"result": 1}
