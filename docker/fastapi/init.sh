8056440b9cd96328831ad91f0cd97883dd7cbb2a
Rudolf0328 <ck07160@hanyang.ac.kr>
2023-02-10 21:24:49 +0900

    docker setting

--- docker/fastapi/init.sh
+++ docker/fastapi/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker rm tungtung_fastapi
+
+docker build -f Dockerfile -t tungtung:first .
 cd ../../
-docker build -f Dockerfile -t tungtung:first . && docker run -p 8000:8000 -v "$(pwd)":/workspace --name tungtung_fastapi --restart unless-stopped tungtung:first
+docker run -p 8000:8000 -v "$(pwd)":/workspace --name tungtung_fastapi --restart unless-stopped tungtung:first
