7139c8d0536b47137bf8f6d308577a326d7e3265
Rudolf0328 <ck07160@hanyang.ac.kr>
2023-03-22 18:07:48 +0900

    docker setting

--- firebase/send.py
+++ firebase/send.py
@@ -2,19 +2,21 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
 
-cred_path = "hyu-around-firebase-adminsdk-hwbpd-acbbacf5a4.json"
+cred_path = "firebase/hyu-around-firebase-adminsdk-hwbpd-acbbacf5a4.json"
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
 # 이 토큰을 프론트에서 줘야 되는 것 같다... 아마도...
-registration_token = 'cBPtDKvuSBK1C7jsu9j5Tk:APA91bE52VQRECUCAILH4XVmLcmMuISbV9QTHZgr3II_03cbuSha9a-MBl5xgop9jlQ6-tSdBRumj5zu8pdwCgvK2ySQXt-RMTQveK34pQjSePRf8TkBtrKDpRK6r27dpPzwA0pSSLIM'
-message = messaging.Message(
-    notification=messaging.Notification(
-        title='title',
-        body='body'
-    ),
-    token=registration_token,
-)
 
-response = messaging.send(message)
-print('Successfully sent message:', response)
+
+def send_message(fcm_token, title, body):
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body
+        ),
+        token=fcm_token,
+    )
+
+    response = messaging.send(message)
+    print('Successfully sent message:', response)
--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from routers import promise, user, auth, category
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("firebase/hyu-around-firebase-adminsdk-hwbpd-acbbacf5a4.json")
-firebase_admin.initialize_app(cred)
+from firebase.send import send_message
+
+registration_token = 'fRd8eXP7RISDQZ4qHQCi_m:APA91bEoyZJo4qkMeK3Ti_jgPgmZnNJnU0fMAAEIJ2rR9rtSh9kXg_2bsA9p-REqby3vua49dj3PxDgesvrJjv4gKC6t8Kj3hN1et125muzrNtnwa6TKv2AK8yrEw36J4zVHJXizI0G1'
+
+
+#
+# cred = credentials.Certificate("firebase/hyu-around-firebase-adminsdk-hwbpd-acbbacf5a4.json")
+# firebase_admin.initialize_app(cred)
 
 app = FastAPI()
 
@@ -14,6 +20,8 @@ app.include_router(promise.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(category.router)
+
+
 
 @app.on_event("startup")
 def on_startup():
@@ -26,4 +34,5 @@ async def root():
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
+    send_message(fcm_token=registration_token, title='title', body='body')
     return {"message": f"Hello {name}"}
