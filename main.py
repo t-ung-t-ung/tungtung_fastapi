515bf2c32fe31fce1d4a28471659e03b0e12c58f
SeoSiun <siun0816@hanyang.ac.kr>
2023-03-22 19:16:36 +0900

    chat post api 구현
    1. chat post api 구현
    2. Chat.time의 default를 현재 시간으로 설정

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from database.database import init_database
-from routers import promise, user, auth, category
+from routers import promise, user, auth, category, chat
 
 import firebase_admin
 from firebase_admin import credentials
@@ -8,7 +8,6 @@ from firebase_admin import credentials
 from firebase.send import send_message
 
 registration_token = 'fRd8eXP7RISDQZ4qHQCi_m:APA91bEoyZJo4qkMeK3Ti_jgPgmZnNJnU0fMAAEIJ2rR9rtSh9kXg_2bsA9p-REqby3vua49dj3PxDgesvrJjv4gKC6t8Kj3hN1et125muzrNtnwa6TKv2AK8yrEw36J4zVHJXizI0G1'
-
 
 #
 # cred = credentials.Certificate("firebase/hyu-around-firebase-adminsdk-hwbpd-acbbacf5a4.json")
@@ -20,11 +19,13 @@ app.include_router(promise.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(category.router)
+app.include_router(chat.router)
 
 
 @app.on_event("startup")
 def on_startup():
     init_database()
+
 
 @app.get("/")
 async def root():
---
+++ routers/chat.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, status, Body, HTTPException
+from sqlmodel import Session, select
+
+from database.scheme_around import Chat
+from database.database import engine
+
+
+router = APIRouter(
+    prefix="/chat"
+)
+
+
+@router.post("/", response_model=Chat, status_code=status.HTTP_201_CREATED, tags=['chat'])
+async def create_chat(chat: Chat = Body(
+    example=Chat(
+        type=0,
+        user_id=1,
+        promise_id=1,
+        content="content"
+    ).json(exclude_none=True)
+)):
+    with Session(engine) as session:
+        session.add(chat)
+        session.commit()
+        session.refresh(chat)
+        return chat
